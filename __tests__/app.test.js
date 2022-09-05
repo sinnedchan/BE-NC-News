f38697c1e40107fa27a0a1db0b030599dea95394
sinnedchan <dench0416@gmail.com>
2022-09-05 16:33:30 +0100

    cleaned up happy path test - all covered in one test with multiple expects, moved errors into app.test.js as a new describe block

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,35 +9,40 @@ afterAll(() => db.end());
 
 describe("GET /api/topics", () => {
   describe("api calls", () => {
-    it("responds with status code of 200", () => {
-      return request(app).get("/api/topics").expect(200);
-    });
-    it("reponds with an object containing an array", () => {
+    it("responds with a status code of 200 and an object containing an array with a length of 3, each topic should be an object with 2 keys (slug + description), both having strings ", () => {
       return request(app)
         .get("/api/topics")
         .expect(200)
         .then(({ body }) => {
-          expect(Array.isArray(body.topics)).toBe(true);
-        });
-    });
-    it("array should be length of 3", () => {
-      return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then(({ body }) => {
+          expect(200);
           expect(body.topics.length).toBe(3);
+          expect(Array.isArray(body.topics)).toBe(true);
+          expect(
+            body.topics.forEach((topic) => {
+              expect(topic).toEqual({
+                slug: expect.any(String),
+                description: expect.any(String),
+              });
+            })
+          );
         });
     });
-    it("each topic should be an object with 2 keys (slug + description), both having strings", () => {
+  });
+});
+
+describe("global errors", () => {
+  describe("404 endpoint not found", () => {
+    it("should return a status code of 404 if trying to access and endpoint that doesn't exist", () => {
+      return request(app).get("/invalidpath").expect(404);
+    });
+    it("return an object with a status code and a message", () => {
       return request(app)
-        .get("/api/topics")
-        .expect(200)
+        .get("/invalidpath")
+        .expect(404)
         .then(({ body }) => {
-          body.topics.forEach((topic) => {
-            expect(topic).toEqual({
-              slug: expect.any(String),
-              description: expect.any(String),
-            });
+          expect(body).toEqual({
+            status: 404,
+            msg: "Error: Endpoint (/invalidpath) not found.",
           });
         });
     });
