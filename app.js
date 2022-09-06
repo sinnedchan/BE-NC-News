8fcdf74f77dd451092e09b8bc45868b3556d4425
sinnedchan <dench0416@gmail.com>
2022-09-06 12:39:48 +0100

    logic for GET /api/articles/:article_id with happy + sad path testing

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getTopics } = require("./controllers/get.controller");
+const { getTopics, getArticlesById } = require("./controllers/get.controller");
 const app = express();
 app.use(express.json());
 
 //GET
 app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticlesById);
 
 //404
 app.get("/*", (req, res) => {
@@ -13,4 +14,31 @@ app.get("/*", (req, res) => {
     .send({ status: 404, msg: `Error: Endpoint (${req.path}) not found.` });
 });
 
+//error handling
+
+//custom errors
+app.use((err, req, res, next) => {
+  switch (err.status) {
+    case 404:
+      res.status(404).send({ status: 404, msg: err.msg });
+  }
+  next(err);
+});
+
+//PSQL errors - based on err code given by PSQL
+app.use((err, req, res, next) => {
+  switch (err.code) {
+    case "22P02":
+      res
+        .status(400)
+        .send({ status: 400, msg: "400: article_id must be a number" });
+  }
+  next();
+});
+
+//server errors
+app.use((err, req, res, next) => {
+  console.log("Error: 500 internal server error");
+  res.status(500).send({ status: 500, msg: "Error 500: internal server" });
+});
 module.exports = app;
--- models/get.model.js
+++ models/get.model.js
@@ -6,3 +6,19 @@ exports.fetchTopics = () => {
     return rows;
   });
 };
+
+exports.fetchArticlesById = (id) => {
+  const query = `
+    SELECT users.name AS author, title, article_id, body, topic, created_at, votes
+    FROM articles
+    JOIN users
+    ON users.username = articles.author
+    WHERE article_id = $1`;
+  return db.query(query, [id]).then(({ rows }) => {
+    if (rows[0]) return rows[0];
+    return Promise.reject({
+      status: 404,
+      msg: `404: no article found with article_id ${id}`,
+    });
+  });
+};
